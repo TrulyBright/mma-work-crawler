7f7798057afb3291c7c4b35cb252ac7861ae7385
TrulyBright <trulybright@yonsei.ac.kr>
2023-06-17 15:38:15 +0900

    basic filter

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,44 +1,72 @@
 <script setup lang="ts">
-import MainTitle from "@/components/MainTitle.vue";
+import MainTitle from "@/components/MainTitle.vue"
+</script>
+<script lang="ts">
+import {API_URL, Job} from "@/shared"
+export default {
+    name: "HomeView",
+    components: {
+        MainTitle,
+    },
+    data() {
+        return {
+            jobs: Array<Job>(),
+            queried: new Job()
+        }
+    },
+    async mounted() {
+        await this.fetch()
+    },
+    methods: {
+        async fetch() {
+            const response = await fetch(API_URL)
+            const parsed: [Object] = await response.json()
+            parsed.forEach((obj: Object) => {
+                const job = new Job()
+                Object.assign(job, obj)
+                this.jobs.push(job)
+            })
+        },
+        async search() {
+            this.jobs.forEach((job) => {
+                job.visible = true
+                Job.keys_for_select_tag().forEach((key) => {
+                    const queried_value = this.queried[key as keyof Job]
+                    const job_value = job[key as keyof Job]
+                    job.visible &&= (!queried_value || queried_value === job_value)
+                })
+            })
+        },
+        optionPool(optionName: string) {
+            const pool = new Set<string | number | boolean>()
+            this.jobs.forEach((job) => {
+                pool.add(job[optionName as keyof typeof job])
+            })
+            return pool
+        }
+    },
+}
 </script>
 <template>
     <MainTitle></MainTitle>
-    <div id="panel">
-        <select name="복무형태">
-            <option value="산업기능요원">산업기능요원</option>
-            <option value="전문연구요원">전문연구요원</option>
-        </select>
-        <select name="지역">
-            <option value="전체">전체</option>
-            <option value="서울">서울</option>
-            <option value="경기">경기</option>
-            <option value="인천">인천</option>
-            <option value="강원">강원</option>
-            <option value="충북">충북</option>
-            <option value="충남">충남</option>
-            <option value="대전">대전</option>
-            <option value="경북">경북</option>
-            <option value="경남">경남</option>
-            <option value="대구">대구</option>
-            <option value="울산">울산</option>
-            <option value="부산">부산</option>
-            <option value="전북">전북</option>
-            <option value="전남">전남</option>
-            <option value="광주">광주</option>
-            <option value="제주">제주</option>
-        </select>
-        <select name="역종">
-            <option value="보충역">보충역</option>
-            <option value="현역">현역</option>
-            <option value="전체">전체</option>
-        </select>
-        <div>
-            <label for="전직자채용가능">전직자 채용가능</label><input type="checkbox" name="전직자채용가능">
-        </div>
-        <div>
-            <input type="text" inputmode="text" name="업체명" placeholder="업체명">
-        </div>
-</div>
+    <div id="filter-panel">
+        <template v-for="entry in Job.keys_for_select_tag()" :key="entry">
+            <label :for="entry">{{ entry }}</label>
+            <select name="key" v-model="queried[entry as keyof typeof queried]" @change="search">
+                <option value="" selected>전체</option>
+                <option
+                v-for="option in optionPool(entry)"
+                :value="option"
+                :key="String(option)"
+                >{{ option }}</option>
+            </select>
+        </template>
+    </div>
+    <ul id="result">
+        <template v-for="job in jobs" :key="job.id">
+        <li v-if="job.visible">{{ job }}</li>
+        </template>
+    </ul>
 </template>
 <style>
 #app {
@@ -47,5 +75,8 @@ import MainTitle from "@/components/MainTitle.vue";
     align-items: center;
     justify-content: center;
     height: 100vh;
+}
+#result {
+    overflow: auto;
 }
 </style>
