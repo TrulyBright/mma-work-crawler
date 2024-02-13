aff4992d2e64257112f7fcdd3798774614579471
TrulyBright <trulybright@yonsei.ac.kr>
2024-02-13 15:50:12 +0800

    delete build_pool()

--- fetch.py
+++ fetch.py
@@ -51,43 +51,22 @@ def seperate복리후생(data: list[dict]):
             item["복리후생"] = item["복리후생"].split(", ")
     return data
 
-def build_entry_pool(data: list[dict]):
-    logging.info("Building entry pool...")
-    pool = dict()
-    with open("postprocessdata.json") as f:
-        loaded = json.load(f)
-        mapping생성항목: dict = loaded["mapping생성항목"]
-        다중선택항목: dict = loaded["다중선택항목"]
-    for item in data:
-        for key, value in item.items():
-            if 대응되는국문명 := mapping생성항목.get(key):
-                if pool.get(대응되는국문명) is None:
-                    pool[대응되는국문명] = dict()
-                pool[대응되는국문명][value] = item[대응되는국문명]
-            elif key in 다중선택항목:
-                pool[key] = pool.get(key, set())
-                pool[key].update(value)
-    return {key: sorted(value) if isinstance(value, set) else dict(sorted(value.items())) for key, value in pool.items()}
-
-def save_with_pool(data: list[dict]):
-    logging.info("Saving data with its entry pool...")
+def save(data: list[dict]):
+    logging.info("Saving data...")
     os.makedirs("front/data", exist_ok=True)
-    with open("front/data/채용공고목록.json", "w") as f, open("front/data/속성풀.json", "w") as p:
+    with open("front/data/채용공고목록.json", "w") as f:
         json.dump(data, f, ensure_ascii=False)
-        json.dump(build_entry_pool(data), p, ensure_ascii=False)
 
 def run():
     logging.basicConfig(level=logging.INFO)
     endpoint = get_api_endpoint()
     key = get_api_key()
     data = fetch(endpoint, key)
-    # with open("front/openings.xml") as f:
-    #     data = f.read()
     parsed = parse(data)
     translated = translate(parsed)
     numberized = numberize(translated)
     seperated = seperate복리후생(numberized)
-    save_with_pool(seperated)
+    save(seperated)
 
 if __name__ == "__main__":
     run()
