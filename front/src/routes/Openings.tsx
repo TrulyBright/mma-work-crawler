d55790481f4b9ce24909b806f26f91430b88042e
TrulyBright <trulybright@yonsei.ac.kr>
2024-02-15 19:45:15 +0800

    상세검색

--- front/src/routes/Openings.tsx
+++ front/src/routes/Openings.tsx
@@ -1,83 +1,79 @@
-import { FormControlLabel, FormGroup, Grid, Checkbox, Typography, FormControl, FormLabel, Card, CardContent, CardHeader, Stack } from "@mui/material"
+import { FormControlLabel, FormGroup, Grid, Checkbox, Typography, FormControl, FormLabel, Card, CardContent, CardHeader, Stack, CardActions, Box, Button, Collapse } from "@mui/material"
 import 채용공고목록 from "../../data/채용공고목록.json"
 import 속성풀 from "../../data/속성풀.json"
+import { ExpandMore } from "@mui/icons-material"
+import React from "react"
 
-const 풀 = {}
+const 주요검색순서 = [
+    "역종",
+    "요원",
+    "업종",
+    "급여",
+    "전직자 채용가능"
+]
 
-Object.entries(속성풀).forEach(([속성명, 후보]) => {
-    if (속성명 === "복리후생") 풀[속성명] = 후보
-    else {
-        풀[속성명] = {}
-        Object.entries(후보).forEach(([코드, 국문명]) => {
-            풀[속성명][Number(코드)] = 국문명
-        })
-    }
-})
+const 상세검색순서 = [
+    "최종학력",
+    "전공",
+    "경력구분",
+    "경력년수",
+    "외국어",
+    "외국어구사능력",
+    "자격증",
+    "수습기간",
+    "근무형태",
+    "출퇴근시간",
+    "교대근무",
+    "특근·잔업",
+    "복리후생",
+    "식사(비)지급",
+    "퇴직금지급",
+]
+
+const gridcheckboxesByEntries = (entries: string[]) => {
+    return entries.map((entry) => (
+        <FormControl key={entry} component="fieldset">
+            <FormLabel component="legend"><strong>{entry}</strong></FormLabel>
+            <Grid container columns={{xs: 1, sm: 2, md: 2}} sx={{maxHeight: "50vh", overflow: "auto"}}>
+                {속성풀[entry].map((속성) => (
+                    <Grid item xs={1} sm={1} md={1} key={속성}>
+                        <FormControlLabel key={속성} control={<Checkbox size="small" />} label={속성} />
+                    </Grid>
+                ))}
+            </Grid>
+        </FormControl>
+    ))
+}
 
 export default () => {
+    const [expanded, setExpanded] = React.useState(false)
     return (
         <>
         <Card>
-            <CardHeader title="채용공고검색조건" />
+            <CardHeader title="검색조건" />
             <CardContent>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">역종</FormLabel>
-                    <FormGroup row>
-                        {Object.entries(풀.역종).map(([코드, 역종]) => (
-                            <FormControlLabel key={코드} control={<Checkbox size="small" />} label={역종} />
-                        ))}
-                    </FormGroup>
-                </FormControl>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">요원</FormLabel>
-                    <FormGroup row>
-                        {Object.entries(풀.요원).map(([코드, 요원]) => (
-                            <FormControlLabel key={코드} control={<Checkbox size="small" />} label={요원} />
-                        ))}
-                    </FormGroup>
-                </FormControl>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">업종</FormLabel>
-                    <Grid container columns={{xs: 4, sm: 8, md: 12}} sx={{maxHeight: "50vh", overflow: "auto"}}>
-                        {Object.entries(풀.업종).map(([코드, 업종]) => (
-                            <Grid item xs={2} sm={4} md={4} key={코드}>
-                                <FormControlLabel key={코드} control={<Checkbox size="small" />} label={업종} />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </FormControl>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">전공</FormLabel>
-                    <FormGroup row>
-                        {Object.entries(풀.전공).map(([코드, 전공]) => (
-                            <FormControlLabel key={코드} control={<Checkbox size="small" />} label={전공} />
-                        ))}
-                    </FormGroup>
-                </FormControl>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">급여</FormLabel>
-                    <Grid container columns={{xs: 4, sm: 8, md: 12}} sx={{maxHeight: "50vh", overflow: "auto"}}>
-                        {Object.entries(풀.급여).map(([코드, 급여]) => (
-                            <Grid item xs={2} sm={4} md={4} key={코드}>
-                                <FormControlLabel key={코드} control={<Checkbox size="small" />} label={급여} />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </FormControl>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">복리후생</FormLabel>
-                    <Grid container columns={{xs: 4, sm: 8, md: 12}} sx={{maxHeight: "50vh", overflow: "auto"}}>
-                        {Object.entries(풀.복리후생).map(([코드, 복지]) => (
-                            <Grid item xs={2} sm={4} md={4} key={코드}>
-                                <FormControlLabel key={코드} control={<Checkbox size="small" />} label={복지} />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </FormControl>
+                {gridcheckboxesByEntries(주요검색순서)}
             </CardContent>
+            <CardActions sx={{display: "flex", justifyContent: "center"}}>
+                <Button sx={{display: "flex", flexDirection: "column", alignItems: "center"}} onClick={() => setExpanded(!expanded)}>
+                    상세검색 펼치기
+                    <ExpandMore sx={{transform: expanded ? "rotate(180deg)" : "rotate(0deg)", transition: "all 0.3s linear"}} />
+                </Button>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    {gridcheckboxesByEntries(상세검색순서)}
+                </CardContent>
+                <CardActions sx={{display: "flex", justifyContent: "center"}}>
+                    <Button sx={{display: "flex", flexDirection: "column", alignItems: "center"}} onClick={() => setExpanded(!expanded)}>
+                        상세검색 접기
+                        <ExpandMore sx={{transform: expanded ? "rotate(180deg)" : "rotate(0deg)", transition: "all 0.3s linear"}} />
+                    </Button>
+                </CardActions>
+            </Collapse>
         </Card>
         <Typography variant="h6" gutterBottom>공고가 {채용공고목록.length}개 있습니다.</Typography>
-        <Stack spacing={2}>
+        {/* <Stack spacing={2}>
             {채용공고목록.map((채용공고, i) => (
                 <Card key={i}>
                     <CardHeader title={채용공고.공고제목} subheader={채용공고.업종} />
@@ -89,7 +85,7 @@ export default () => {
                     </CardContent>
                 </Card>
             ))}
-        </Stack>
+        </Stack> */}
         </>
     )
 }
